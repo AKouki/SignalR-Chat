5cfd9f3d37b2193fe436cfe092574aa04347128e
Albion Kouki <koukialbion@yahoo.com>
2022-09-02 20:14:59 +0300

    Updated Startup

--- Chat.Web/Startup.cs
+++ Chat.Web/Startup.cs
@@ -32,9 +32,6 @@ namespace Chat.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("SignalRChatDB"));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
